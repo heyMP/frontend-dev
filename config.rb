d3422c7363454a905641410733609fd38fffeae7
Michael Potter <mgp140@psu.edu>
2014-11-16 22:50:43 -0500

    Added Compass Normalize.

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ require 'singularitygs'
 require 'toolkit'
 require 'modular-scale'
 require 'color-schemer'
+require 'compass-normalize'
 
 # Set this to the root of your project when deployed:
 http_path = "./"
