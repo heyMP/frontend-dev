7b9321965ffa2d31027a292a18763d3066a5d95c
Michael Potter <mgp140@gmail.com>
2015-03-09 22:58:26 -0400

    Added object-fit library and added an example 'slick header' component as an example. #17

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 (function ($) {
   'use strict';
 
+  objectFit.polyfill({
+      selector: 'img', // this can be any CSS selector
+      fittype: 'cover', // either contain, cover, fill or none
+      disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
+  });
+
 })(jQuery);
