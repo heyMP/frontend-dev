70a7a829cd6dc49483b34f5381f84a158e346594
Michael Potter <mgp140@gmail.com>
2015-08-02 14:55:04 -0400

    Additional examples using Browsersync, hologram, and BEM modifiers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var viewportUnitsRepaint = require('./viewportUnitsRepaint.js');
+
 (function ($) {
   'use strict';
 
@@ -6,5 +8,19 @@
       fittype: 'cover', // either contain, cover, fill or none
       disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
   });
+
+  // MenuToggle
+  if (typeof Drupal != 'undefined') {
+    Drupal.behaviors.repaintSlickbanner = {
+      attach: function (context, settings) {
+        viewportUnitsRepaint($('.slickheader__title'));
+      }
+    };
+  }
+  else {
+    $(document).ready(function() {
+      viewportUnitsRepaint($('.slickheader__title'));
+    });
+  }
 
 })(jQuery);
